9d02bcaafc8143438e751b9530d5c2caf6be659a
Mohit_100 <mohithedaoo2000@gmail.com>
2024-12-19 19:37:00 +0530

    updated Floyds Triangle code

--- 01_Pattern_Practice_Questions/09_Floyds_Triangle.py
+++ 01_Pattern_Practice_Questions/09_Floyds_Triangle.py
@@ -1,8 +1,15 @@
 def generate_floyds_triangle(n):
-    row = []
+    triangle = []
+    num = 1
 
-    for i in range(n):
-        row.append()
+    for i in range(1, n + 1):
+        row = ""
+        for j in range(i):
+            row += str(num) + " "
+            num += 1
+        triangle.append(row.strip())  # Remove trailing whitespace
 
-pattern = generate_floyds_triangle(4)
+    return triangle
+
+pattern = generate_floyds_triangle(5)
 print(pattern)
